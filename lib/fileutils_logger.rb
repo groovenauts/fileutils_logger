40d52d079883114ae153b0e7177f7874a9bf1826
akima <akm2000@gmail.com>
2014-08-29 14:28:41 +0900

    copied from scm_workspace:
    https://github.com/groovenauts/scm_workspace/tree/cbf4c3eeae74f67ed55165b92ae6bbd72652820f

--- lib/fileutils_logger.rb
+++ lib/fileutils_logger.rb
@@ -1,5 +1,61 @@
 require "fileutils_logger/version"
 
-module FileutilsLogger
-  # Your code goes here...
+require 'fileutils'
+require 'logger'
+
+module FileUtils
+
+  # Monkey patch
+  # original source code is here:
+  #     https://github.com/ruby/ruby/blob/v2_0_0_247/lib/fileutils.rb#L122
+  def cd(dir, options = {}, &block) # :yield: dir
+    fu_check_options options, OPT_TABLE['cd']
+    fu_output_message "cd #{dir}" if options[:verbose]
+    r = Dir.chdir(dir, &block)
+    fu_output_message 'cd -' if options[:verbose] and block
+    return r # this is difference from original and this is important!
+  end
+  module_function :cd
+
+  alias chdir cd
+  module_function :chdir
+
+  class << self
+    def with_logger(logger, level = :info)
+      output = LoggerAdapter.new(logger, level)
+
+      Module.new do
+        include FileUtils
+        @fileutils_output  = output
+        @fileutils_label   = ''
+
+        ::FileUtils.collect_method(:verbose).each do |name|
+          module_eval(<<-EOS, __FILE__, __LINE__ + 1)
+            def #{name}(*args)
+              super(*fu_update_option(args, :verbose => true))
+            end
+            private :#{name}
+          EOS
+        end
+
+        extend self
+        class << self
+          ::FileUtils::METHODS.each do |m|
+            public m
+          end
+        end
+      end
+
+    end
+  end
+
+  class LoggerAdapter
+    def initialize(logger, level)
+      @logger, @level = logger, level
+    end
+    def puts(msg)
+      @logger.send(@level, msg)
+    end
+  end
+
 end
--- spec/fileutils_logger_spec.rb
+++ spec/fileutils_logger_spec.rb
@@ -1,11 +1,73 @@
 require 'spec_helper'
 
-describe FileutilsLogger do
+require "stringio"
+require "tmpdir"
+
+describe FileUtils do
   it 'has a version number' do
     expect(FileutilsLogger::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:io){ StringIO.new }
+  let(:logger){ Logger.new(io) }
+  let(:fu){ FileUtils.with_logger(logger) }
+
+  describe :chdir do
+    it "default" do
+      dir = File.expand_path("../..", __FILE__)
+      fu.chdir(dir) do
+        fu.pwd.should == dir
+      end
+      io.rewind
+      lines = io.read.lines
+      lines.length.should == 2
+      lines[0].should =~ /\AI, \[.+\]\s+INFO -- : cd #{Regexp.escape(dir)}/
+      lines[1].should =~ /\AI, \[.+\]\s+INFO -- : cd -/
+    end
+
+    it "debug" do
+      debug_fu = FileUtils.with_logger(logger, :debug)
+      dir = File.expand_path("../..", __FILE__)
+      debug_fu.chdir(dir) do
+        debug_fu.pwd.should == dir
+      end
+      io.rewind
+      lines = io.read.lines
+      lines.length.should == 2
+      lines[0].should =~ /\AD, \[.+\]\s+DEBUG -- : cd #{Regexp.escape(dir)}/
+      lines[1].should =~ /\AD, \[.+\]\s+DEBUG -- : cd -/
+    end
   end
+
+  describe :mkdir_p do
+    it "default" do
+      path = nil
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, "foo/bar")
+        fu.mkdir_p(path)
+        Dir.exist?(path).should eq true
+      end
+      io.rewind
+      lines = io.read.lines
+      lines.length.should == 1
+      lines[0].should =~ /\AI, \[.+\]\s+INFO -- : mkdir -p #{Regexp.escape(path)}/
+    end
+  end
+
+  describe :touch do
+    it "default" do
+      path = nil
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, "restart.txt")
+        fu.touch(path)
+        File.exist?(path).should eq true
+        IO.read(path).should == ""
+      end
+      io.rewind
+      lines = io.read.lines
+      lines.length.should == 1
+      lines[0].should =~ /\AI, \[.+\]\s+INFO -- : touch #{Regexp.escape(path)}/
+    end
+  end
+
 end
